94e4fc4d00030ef4bdd9ebc3cff42b6da33a9b79
Anhad Jai Singh <anhad.jai.singh@gmail.com>
2013-04-12 23:12:12 +0530

    Basic utils

---
+++ src/randShuffle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import random
+
+
+class Numbers:
+
+    def __init__(self)
+        x = range(0,10)
+        random.shuffle(x)
+        self.numbers = x
+
+    def sortNumbers(self):
+        self.numbers.sort()
